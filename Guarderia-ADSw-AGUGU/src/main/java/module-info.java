69331c3637bc8464ecf1339b17ff1db09d3d0ad6
Kevin <kevinricog@javeriana.edu.co>
2026-05-28 09:48:41 -0500

    commit

--- Guarderia-ADSw-AGUGU/src/main/java/module-info.java
+++ Guarderia-ADSw-AGUGU/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module co.edu.javeriana.guarderiaadswagugu {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.google.gson;
 
     opens co.edu.javeriana.guarderiaadswagugu to javafx.fxml;
     opens co.edu.javeriana.guarderiaadswagugu.modelo.ninos to com.google.gson;
